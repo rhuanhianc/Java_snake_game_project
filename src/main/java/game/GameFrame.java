e9c2e6deec97e7db7438f5c8908b68cf9ab19a0c
Rhuan S. Hianc <rhuanhianc123@gmail.com>
2022-02-27 18:12:13 -0300

    Create GameFrame.java

---
+++ src/main/java/game/GameFrame.java
@@ -0,0 +1,16 @@
+package game;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    public GameFrame() {
+        this.add(new GamePanel());
+        this.setTitle("Game Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setSize(400, 400);
+    }
+}
